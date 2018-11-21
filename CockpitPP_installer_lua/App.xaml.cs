96b963d107ab85238fe82f46ade85449e012338a
Nicolas HERR <nicolas.herr68@gmail.com>
2018-11-21 20:47:39 +0100

    test

---
+++ CockpitPP_installer_lua/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CockpitPP_installer_lua
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+
+
+    }
+}
